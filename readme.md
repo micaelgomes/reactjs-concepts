f270160ac1a346d19a79b1b70992549f57018e90
Micael Gomes <micaellgoms@gmail.com>
2020-08-21 18:08:56 -0300

    feat(interface): using axios to get api's data

---
+++ readme.md
@@ -0,0 +1,22 @@
+# 2nd chalenge complete ![complete](https://camo.githubusercontent.com/facfcb6afd684d2c9701c7d6add65f391fdf86fc/68747470733a2f2f696d672e736869656c64732e696f2f636f6465636f762f632f6769746875622f6477796c2f686170692d617574682d6a7774322e7376673f6d61784167653d32353932303030)
+
+## :rocket: Sobre o desafio
+
+Esta aplicação irá fornecer uma interface para os repositórios do 1º defafio, que é uma REST API feita em Node.js.
+
+### Específicação dos testes
+
+Em cada teste, tem uma breve descrição no que sua aplicação deve cumprir para que o teste passe.
+
+Caso você tenha dúvidas quanto ao que são os testes, e como interpretá-los, dê uma olhada em **[nosso FAQ](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/faq-desafios).**
+
+Para esse desafio temos os seguintes testes:
+
+- **`should be able to add new repository`**: Para que esse teste passe, sua aplicação deve permitir que um repositório seja adicionado ao seu backend e listado no seu frontend dentro de uma `LI`.
+
+- **`should be able to remove repository`**: Para que esse teste passe, sua aplicação deve permitir que ao clicar no botão de remover que vai estar dentro da `LI` do repositório adicionado, o item seja removido da listagem.
+
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  // 1st shoot
   async function handleAddRepository() {
-    // TODO
+    const response = await api
+      .post("repositories", {
+        title: `react in ${Date.now()}`,
+        url: "https://github.com/micaelgoms/Agente12",
+        techs: ["node", "react", "CSS"],
+      })
+      .catch((reject) => console.error(reject));
+
+    const newRepo = response.data;
+    setRepositories([...repositories, newRepo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api
+      .delete("repositories/" + id.toString())
+      .then((response) => console.log(response))
+      .catch((reject) => console.error(reject));
+
+    let indexRepo = repositories.findIndex((repo) => repo.id === id);
+    if (indexRepo < 0) return alert("No repository founded!");
+
+    const tmpRepositories = [...repositories];
+    tmpRepositories.splice(indexRepo, 1);
+    setRepositories(tmpRepositories);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
